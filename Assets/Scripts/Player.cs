ce6529237d8ee28a9676f68833da5edeaa3d2daf
sam-turney <122310764+sam-turney@users.noreply.github.com>
2024-05-03 14:30:29 -0700

    Basic camera movement to test block interaction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,6 +20,12 @@ public class Player : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         transform.position += new Vector3(horizontal, 0f, vertical) * Time.deltaTime * speed;
 
+        float MouseX = Input.GetAxis("Mouse X");
+        float MouseY = Input.GetAxis("Mouse Y");
+
+        gameObject.transform.eulerAngles += new Vector3(-MouseY, MouseX, 0);
+        playerCamera.transform.LookAt(gameObject.transform);
+
         if (Input.GetKeyDown(KeyCode.F))
         {
             Vector3[] voxelInfo = looking.LookingAt(playerCamera);
