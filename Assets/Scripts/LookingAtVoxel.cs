ca935dfb8f870ad30868ffd573da7d9b61da4b92
sam-turney <122310764+sam-turney@users.noreply.github.com>
2024-05-03 14:08:03 -0700

    Get position of voxel in line of sight

---
+++ Assets/Scripts/LookingAtVoxel.cs
@@ -0,0 +1,36 @@
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class LookingAtVoxel : MonoBehaviour
+{
+    public Camera playerCamera;
+    private LayerMask playerLayer;
+    private Vector3 lookDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerLayer = LayerMask.GetMask("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3[] LookingAt()
+    {
+        lookDir = playerCamera.transform.forward;
+        Debug.DrawRay(playerCamera.transform.position, lookDir * 20);
+        Ray ray = new Ray(playerCamera.transform.position, lookDir);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 20, ~playerLayer))
+        {
+            return new Vector3[] { hit.point, hit.normal };
+        }
+
+        return null;
+    }
+}
