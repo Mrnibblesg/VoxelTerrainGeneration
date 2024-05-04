378fdb64dfa2e57bc650da78ce8a4775223fcf1e
sam-turney <122310764+sam-turney@users.noreply.github.com>
2024-05-03 18:09:19 -0700

    Player script reflects the interaction script move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,17 +3,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 10f;
-    public Camera playerCamera;
-    private LookingAtVoxel looking;
-    private VoxelType.Type currType;
 
     private void Start()
     {
         WorldGenerator w = WorldGenerator.World;
         Vector3 startPosition = new(0.5f, w.worldHeight * w.chunkHeight + 1.5f, 0.5f); 
         transform.position = startPosition;
-        looking = gameObject.AddComponent<LookingAtVoxel>();
-        currType = VoxelType.Type.GRASS;
     }
 
     // Update is called once per frame
@@ -29,94 +24,7 @@ public class Player : MonoBehaviour
         playerCamera.transform.eulerAngles += new Vector3(-MouseY, MouseX, 0);
         // playerCamera.transform.LookAt(gameObject.transform);
 
-        CheckKeyPress();
     }
 
-    private void CheckKeyPress()
-    {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Vector3[] voxelInfo = looking.LookingAt(playerCamera);
-            if (voxelInfo != null)
-            {
-                var position = voxelInfo[0] - (voxelInfo[1] / 2);
-                Voxel? voxel = WorldGenerator.World.GetVoxel(position);
-                if (voxel != null)
-                {
-                    WorldGenerator.World.SetVoxel(position, (Voxel)voxel?.SetType(VoxelType.Type.AIR));
-                }
-            }
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            currType = VoxelType.Type.GRASS;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            currType = VoxelType.Type.DIRT;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            currType = VoxelType.Type.STONE;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            currType = VoxelType.Type.GLASS;
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Vector3[] voxelInfo = looking.LookingAt(playerCamera);
-            if (voxelInfo != null)
-            {
-                var position = voxelInfo[0] + (voxelInfo[1] / 2);
-                Voxel? voxel = WorldGenerator.World.GetVoxel(position);
-                if (voxel != null && ((Voxel)voxel).type == VoxelType.Type.AIR)
-                {
-                    WorldGenerator.World.SetVoxel(position, (Voxel)voxel?.SetType(currType));
-                }
-            }
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Vector3[] voxelInfo = looking.LookingAt(playerCamera);
-            Debug.Log("Looking at point: " + voxelInfo[0]);
-            Debug.Log("Block Normal Vector: " + voxelInfo[1]);
-        }
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            Debug.Log(transform.position);
-        }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3[] voxelInfo = looking.ClickedVoxel(playerCamera);
-            if (voxelInfo != null)
-            {
-                var position = voxelInfo[0] - (voxelInfo[1] / 2);
-                Voxel? voxel = WorldGenerator.World.GetVoxel(position);
-                if (voxel != null)
-                {
-                    WorldGenerator.World.SetVoxel(position, (Voxel)voxel?.SetType(VoxelType.Type.AIR));
-                }
-            }
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            Vector3[] voxelInfo = looking.ClickedVoxel(playerCamera);
-            if (voxelInfo != null)
-            {
-                var position = voxelInfo[0] + (voxelInfo[1] / 2);
-                Voxel? voxel = WorldGenerator.World.GetVoxel(position);
-                if (voxel != null && ((Voxel)voxel).type == VoxelType.Type.AIR)
-                {
-                    WorldGenerator.World.SetVoxel(position, (Voxel)voxel?.SetType(currType));
-                }
-            }
-        }
-    }
+   
 }
