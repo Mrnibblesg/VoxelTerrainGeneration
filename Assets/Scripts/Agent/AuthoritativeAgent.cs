123f3a9d227d7fd12d036f96feaeed1aa503e418
Parker Gutierrez <mrnibblesgutierrez@gmail.com>
2024-06-11 19:26:10 -0700

    Fix chunks not loading until the agent moves over a chunk boundary

--- Assets/Scripts/Agent/AuthoritativeAgent.cs
+++ Assets/Scripts/Agent/AuthoritativeAgent.cs
@@ -37,7 +37,7 @@ public abstract class AuthoritativeAgent : Agent
         set
         {
             currentWorld = value;
-            UpdateChunkCoord();
+            currentWorld?.UpdateAuthAgentChunkPos(this);
         }
     }
 
